32ef3c9d4e22baeb38ad3f6832057ee32efd7cfa
Maliha Ganesh <107932008+MalihaGit@users.noreply.github.com>
2022-07-02 12:17:01 +0000

    commited final.py

--- final.py
+++ final.py
@@ -1,1 +1,9 @@
+from flask import Flask
+app=Flask(__name__)
+@app.route("/")
+def welcome():
+  st="Stranger Things is an American science fiction horror drama television series created by the Duffer Brothers that is streaming on Netflix. The brothers serve as showrunners and are executive producers along with Shawn Levy and Dan Cohen."
+  return st
+if __name__=="__main__":
+  app.run("0.0.0.0")
 
